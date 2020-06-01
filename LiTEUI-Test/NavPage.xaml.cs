b35fbb9882c4e071ad9e8aaed2760efe127fe917
Pietro Gagliardi <pietro.gagliardi@outlook.com>
2020-06-01 18:21:39 +0200

    Dimensioni elementi finestra customizzabili e migliorata la struttura dello stile di base delle finestre

--- LiTEUI-Test/NavPage.xaml.cs
+++ LiTEUI-Test/NavPage.xaml.cs
@@ -73,8 +73,12 @@ namespace LiTEUI_Test
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            //NavigationService.Navigate<NavPage>(("Title", Title));
-            new MainWindow().Show();
+            NavigationService.Navigate<NavPage>(("Title", Title));
+            Application.Current.Resources["WindowBarHeight"] = 40d;
+            Application.Current.Resources["WindowButtonWidth"] = 40d;
+            Application.Current.Resources["WindowContentMargin"] = new Thickness(10, 40, 10, 10);
+            //Application.Current.Resources["WindowBarTextFontSize"] = 16d;
+            //Application.Current.Resources["WindowBarSymbolFontSize"] = 14d;
         }
     }
 }
